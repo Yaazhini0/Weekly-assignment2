21773d88e215b56e7cc659cf0d397e47cfb27268
Yaazhini0 <yaazhini.n@gmail.com>
2026-04-10 13:54:31 +0530

    week1and2

---
+++ level1/src/main/java/Level12.java
@@ -0,0 +1,13 @@
+
+class Level12 {
+    public static void main(String[] args){
+        int maths_marks = 94;
+        int physics_marks = 95;
+        int chemistry_marks = 96;
+        int avg_marks;
+        avg_marks = (maths_marks + physics_marks+chemistry_marks)/3;
+        System.out.println("Sam's average mark in PCM is "+avg_marks);
+    }
+}
+
+
