c23c29a5ca45609d16c54994ee67d358174075d9
Yaazhini0 <yaazhini.n@gmail.com>
2026-04-10 14:06:43 +0530

    week1and2

---
+++ level1/src/main/java/Level13.java
@@ -0,0 +1,11 @@
+public class Level13 {
+    public static void main(String[] args){
+        double km = 10.8;
+        double miles;
+        miles = km / 1.6;
+        System.out.println("The distance "+km+"km in miles is "+miles);
+    }
+
+}
+
+
