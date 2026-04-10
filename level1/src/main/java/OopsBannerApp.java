378bb46f3e16618910fc117a46177aed0c960a4c
Yaazhini0 <yaazhini.n@gmail.com>
2026-04-10 13:47:27 +0530

    week1and2

---
+++ level1/src/main/java/OopsBannerApp.java
@@ -0,0 +1,10 @@
+class Level1{
+    public static void main(String[] args){
+        int age;
+        int curr_year = 2024;
+        int birth_year = 2000;
+        age = curr_year - birth_year;
+        System.out.println("Harry's age in 2024 is "+ age);
+    }
+}
+
